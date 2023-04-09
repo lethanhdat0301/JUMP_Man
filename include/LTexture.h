09b0714c5dc2cb631fc16d46523ad36b69b30f11
lethanhdat0301 <125379898+lethanhdat0301@users.noreply.github.com>
2023-04-09 22:37:12 +0700

    Add files via upload

---
+++ include/LTexture.h
@@ -0,0 +1,16 @@
+#ifndef LTEXTURE_H
+#define LTEXTURE_H
+
+
+class LTexture
+{
+    public:
+        LTexture();
+        virtual ~LTexture();
+
+    protected:
+
+    private:
+};
+
+#endif // LTEXTURE_H
